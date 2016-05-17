beac61b7aaafae51ccf411605565d36acbec1705
ragularatnam <ragulara93@gmail.com>
2016-05-17 09:08:23 +0200

    second commit

--- js/cours1.js
+++ js/cours1.js
@@ -1,3 +1,22 @@
-var str = "coucou";
-console.log(str.charAt(0));
-console.log("coucou".charAt(0));
+
+function test() {
+  var str = "coucou";
+  console.log(str.charAt(0));
+  console.log("coucou".charAt(0));
+}
+
+
+
+/*
+Ces 4 déclarations de fonctions sont équivalente et ont la même performance :
+- function a() { console.log(i); }
+- this.a = function() { console.log(i); }
+- a = function() { console.log(i); }
+- var a = function() { console.log(i); }
+*/
+var a = function()
+{
+  var i = 0;
+  i++;
+  console.log(i);
+}
