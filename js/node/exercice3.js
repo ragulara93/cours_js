dc741d1742f518e67d70cc0b25a19bf13066b0f1
ragularatnam <ragulara93@gmail.com>
2016-05-17 16:31:15 +0200

    Debut exercice gestion fichier par nodeJS

---
+++ js/node/exercice3.js
@@ -0,0 +1,11 @@
+// 1 - charger le fichier data.txt
+// lire le contenu
+
+// 2 - afficher tous les utilisateurs sont le prenom commence
+// par L
+
+// 3 : crÃ©er un fichier par groupe d'utilisateur
+// un groupe d'utilisateur par lettre de l'alphabet
+// A.txt -> tous les utilisateurs dont le prÃ©nom commence par A
+// B.txt -> tous les utilisateurs dont le prÃ©nom commence par B
+// ....
